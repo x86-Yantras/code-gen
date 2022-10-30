a0e32dd4663d4aca2b40d1500adef807e20e620b
rolorin <rrb.zeek@gmail.com>
2022-10-30 18:42:26 +0100

    feat(code-gen): config

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/x86-Yantras/code-gen/internal/constants"
+)
+
+type Config struct {
+	ProjectStructure map[string][]string
+	PackageManager   string
+	ServiceDir       string
+	ReadmeFile       string
+}
+
+func New(configType string) (*Config, error) {
+	configFile := fmt.Sprintf("%s/%s%s", constants.ConfigDir, configType, "_config.json")
+
+	configByte, err := os.ReadFile(configFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+
+	err = json.Unmarshal(configByte, config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
