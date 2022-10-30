6d04eb2855f8c397524cec9fd19f13b8a751a77c
rolorin <rrb.zeek@gmail.com>
2022-10-30 18:44:01 +0100

    feat(code-gen): constants

---
+++ internal/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const TemplatesDir = "templates"
+const ConfigDir = "./config"
+const TemplateExtension = ".tmpl"
+
+// Messages
+const ProjectBuiltMsg = `Built %s project, don't forget to commit`
+const UndefinedCommandMsg = `Cannot parse command %s`
