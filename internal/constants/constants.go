2575ab0f6e926b8ae921d7674f9d72b690bf37e0
rolorin <rrb.zeek@gmail.com>
2022-10-31 10:00:36 +0100

    feat(dapp): added service directory and file placeholders and limit and offset value

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,9 +1,19 @@
 package constants
 
+// Dirs
 const TemplatesDir = "templates"
 const ConfigDir = "./config"
+const ServiceDirPlaceholder = `_servicename_`
+const ServiceFilePlaceholder = `_service_`
+
+// Files
 const TemplateExtension = ".tmpl"
 
 // Messages
-const ProjectBuiltMsg = `Built %s project, don't forget to commit`
+const ProjectBuiltMsg = `Built %s, don't forget to commit`
 const UndefinedCommandMsg = `Cannot parse command %s`
+
+// HTTP
+const PayloadLimit = "limit"
+const PayloadOffset = "offset"
+const ContentJson = "application/json"
