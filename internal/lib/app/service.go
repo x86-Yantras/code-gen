d7b3f2e93b965a7aa1855ae6cc25eda370afbd2a
rolorin <rrb.zeek@gmail.com>
2022-10-31 09:58:07 +0100

    feat(dapp): added lib for generating service

---
+++ internal/lib/app/service.go
@@ -0,0 +1,173 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/x86-Yantras/code-gen/internal/constants"
+)
+
+type ServiceModel struct {
+	ServiceName        string
+	ServiceDescription string
+	ServicePayload     interface{}
+}
+
+func (app *App) CreateService() error {
+	for _, doc := range app.Spec.Paths {
+		if doc.Get != nil {
+			err := app.BuildService(doc.Get, http.MethodGet)
+			if err != nil {
+				return err
+			}
+		}
+		if doc.Post != nil {
+			err := app.BuildService(doc.Post, http.MethodPost)
+			if err != nil {
+				return err
+			}
+		}
+		if doc.Patch != nil {
+			err := app.BuildService(doc.Patch, http.MethodPatch)
+			if err != nil {
+				return err
+			}
+		}
+		if doc.Put != nil {
+			err := app.BuildService(doc.Put, http.MethodPut)
+			if err != nil {
+				return err
+			}
+		}
+		if doc.Delete != nil {
+			err := app.BuildService(doc.Delete, http.MethodDelete)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (app *App) BuildService(spec *openapi3.Operation, operationType string) error {
+	// Take first tag as service name
+	serviceName := spec.Tags[0]
+
+	if err := app.BuildDir(serviceName); err != nil {
+		return err
+	}
+
+	service := &ServiceModel{
+		ServiceName:        spec.OperationID,
+		ServiceDescription: spec.Description,
+	}
+	payload := app.BuildPayload(spec, operationType)
+
+	service.ServicePayload = payload
+
+	if err := app.BuildServiceFiles(serviceName, spec.OperationID, service); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) extractParametes(pararms openapi3.Parameters, payloadObject map[string]interface{}) {
+	for _, param := range pararms {
+		payloadObject[param.Value.Name] = app.getDefaultType(param.Value.Schema.Value.Type)
+	}
+}
+
+func (app *App) extractBody(body *openapi3.RequestBodyRef, payloadObject map[string]interface{}) {
+	if body != nil {
+		props := body.Value.Content[constants.ContentJson].Schema.Value.Properties
+		for key, prop := range props {
+			payloadObject[key] = app.getDefaultType(prop.Value.Type)
+		}
+	}
+}
+
+func (app *App) getDefaultType(inType string) interface{} {
+	switch inType {
+	case "string":
+		return ""
+	case "number":
+		return 0
+	case "boolean":
+		return false
+	default:
+		return ""
+	}
+}
+func (s *ServiceModel) Validate() error {
+	if s.ServiceName == "" {
+		return errors.New("invalid spec file, operationId missing")
+	}
+	return nil
+}
+
+func (app *App) BuildPayload(spec *openapi3.Operation, operationType string) map[string]interface{} {
+	var payload map[string]interface{}
+
+	switch operationType {
+	case http.MethodGet:
+		payload = map[string]interface{}{}
+		// default limit and offset
+		payload[constants.PayloadLimit] = 50
+		payload[constants.PayloadOffset] = 0
+		app.extractParametes(spec.Parameters, payload)
+	case http.MethodPost:
+		payload = map[string]interface{}{}
+		app.extractParametes(spec.Parameters, payload)
+		app.extractBody(spec.RequestBody, payload)
+	case http.MethodPatch:
+		payload = map[string]interface{}{}
+		app.extractParametes(spec.Parameters, payload)
+		app.extractBody(spec.RequestBody, payload)
+	case http.MethodPut:
+		payload = map[string]interface{}{}
+		app.extractParametes(spec.Parameters, payload)
+		app.extractBody(spec.RequestBody, payload)
+	case http.MethodDelete:
+		payload = map[string]interface{}{}
+		app.extractParametes(spec.Parameters, payload)
+		app.extractBody(spec.RequestBody, payload)
+	default:
+		payload = map[string]interface{}{}
+		app.extractParametes(spec.Parameters, payload)
+		app.extractBody(spec.RequestBody, payload)
+	}
+	fmt.Println(payload)
+	return payload
+}
+
+func (app *App) BuildDir(serviceName string) error {
+	// create services dir
+	serviceDir := app.Config.ServiceDir
+
+	if err := app.CreateDir(serviceDir); err != nil {
+		return err
+	}
+
+	servicePath := fmt.Sprintf("%s/%s", serviceDir, serviceName)
+	if err := app.CreateDir(strings.ToLower(servicePath)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) BuildServiceFiles(serviceName, operation string, service *ServiceModel) error {
+	serviceDir := app.Config.ServiceDir
+	serviceFilePath := fmt.Sprintf("%s/%s/%s.js", serviceDir, serviceName, operation)
+
+	serviceTemplatePath := fmt.Sprintf("%s/%s/%s/service/%s.js%s", app.AppTemplateDir, serviceDir, constants.ServiceDirPlaceholder, constants.ServiceFilePlaceholder, constants.TemplateExtension)
+
+	if err := app.Templater.Create(serviceFilePath, serviceTemplatePath, service); err != nil {
+		return err
+	}
+	return nil
+}
