9e170cbbf5c3745fd52e4dfff9259fe216252aa9
rolorin <rrb.zeek@gmail.com>
2022-11-13 18:43:01 +0100

    feat(code-gen): replaced with file create params

--- internal/lib/app/template.go
+++ internal/lib/app/template.go
@@ -3,13 +3,18 @@ package app
 import (
 	"strings"
 
+	"github.com/x86-Yantras/code-gen/internal/adapters/templates"
 	"github.com/x86-Yantras/code-gen/internal/constants"
 )
 
 func (app *App) TemplateToFile(templatePath, filepath string, data interface{}) error {
 
 	fileName := strings.Replace(filepath, constants.TemplateExtension, "", 1)
-	err := app.Templater.Create(fileName, templatePath, data)
+	err := app.Templater.Create(&templates.FileCreateParams{
+		FileName:     fileName,
+		TemplatePath: templatePath,
+		Data:         data,
+	})
 
 	if err != nil {
 		return err
