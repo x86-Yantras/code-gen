87dae28e8da2db7c270793cb5cdee04cd60fd733
rolorin <rrb.zeek@gmail.com>
2022-11-13 18:41:00 +0100

    feat(code-gen): added append case for fs and improved payload for create

--- internal/adapters/templates/template.go
+++ internal/adapters/templates/template.go
@@ -7,17 +7,37 @@ import (
 )
 
 type Templates struct{}
+type Files struct {
+	FilePath     string
+	TemplatePath string
+}
 
-func (t *Templates) Create(fileName, templatePath string, data interface{}, overwrite ...bool) error {
-	tpl, err := template.ParseFiles(templatePath)
+type FileCreateParams struct {
+	FileName     string
+	TemplatePath string
+	Data         interface{}
+	Overwrite    bool
+	Append       bool
+}
+
+func (t *Templates) Create(params *FileCreateParams) error {
+	tpl, err := template.ParseFiles(params.TemplatePath)
 
 	if err != nil {
 		return err
 	}
-	_, err = os.Stat(fileName)
 
-	if os.IsNotExist(err) || (len(overwrite) > 0 && overwrite[0]) {
-		file, err := os.Create(fileName)
+	_, err = os.Stat(params.FileName)
+
+	if os.IsNotExist(err) || params.Append || params.Overwrite {
+		fileFlags := os.O_WRONLY | os.O_CREATE
+
+		if params.Append {
+			fileFlags = fileFlags | os.O_APPEND
+			fmt.Printf("appending to file %s\n", params.FileName)
+		}
+
+		file, err := os.OpenFile(params.FileName, fileFlags, 0644)
 
 		if err != nil {
 			return err
@@ -25,12 +45,25 @@ func (t *Templates) Create(fileName, templatePath string, data interface{}, over
 
 		defer file.Close()
 
-		if err := tpl.Execute(file, data); err != nil {
+		if err := tpl.Execute(file, params.Data); err != nil {
 			return err
 		}
 		return nil
 	}
 
-	fmt.Printf("Skipping: file %s already exists\n", fileName)
+	fmt.Printf("Skipping: file %s already exists\n", params.FileName)
+	return nil
+}
+
+func (t *Templates) CreateMany(data interface{}, files ...*Files) error {
+	for _, file := range files {
+		if err := t.Create(&FileCreateParams{
+			FileName:     file.FilePath,
+			TemplatePath: file.TemplatePath,
+			Data:         data,
+		}); err != nil {
+			return err
+		}
+	}
 	return nil
 }
