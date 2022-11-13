7e4fb1fa0c87d627cb0fd0ed65d9c36e0b842f4f
rolorin <rrb.zeek@gmail.com>
2022-11-13 18:41:23 +0100

    feat(code-gen): added constants for storage

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -19,4 +19,14 @@ const PayloadLimit = "limit"
 const PayloadOffset = "offset"
 const ContentJson = "application/json"
 const APIHTTPAdapter = "api/http"
+
+// Storage
+const Storage = "storage"
+const StorageEntityPlaceholder = "_entity_"
+const StorageEntityOpPlaceholder = "_entity_op_"
+
+// To-do(rolorin): move this to cli ops
+const DefaultStorage = "mongo"
+
+// misc
 const Service = "service"
