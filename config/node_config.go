926833b7e4030134dfeff46594d61deec1459d9b
rolorin <rrb.zeek@gmail.com>
2022-11-15 19:04:23 +0100

    feat(code-gen): added config for lib directory

--- config/node_config.go
+++ config/node_config.go
@@ -3,6 +3,7 @@ package config
 func NodeConfig() *Config {
 	return &Config{
 		ServiceDir:     "src/services",
+		LibDir:         "src/lib",
 		AdapterDir:     "src/services/%s/adapters",
 		PackageManager: "package.json",
 		ReadmeFile:     "Readme.md",
