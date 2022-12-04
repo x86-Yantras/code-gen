5b20f45590552d5aa39b8e440cec1f45317996df
rolorin <rrb.zeek@gmail.com>
2022-12-04 17:28:29 +0100

    feat(service-param): adding service param

--- internal/lib/app/app.go
+++ internal/lib/app/app.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"embed"
+	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/x86-Yantras/code-gen/config"
@@ -19,11 +21,18 @@ type App struct {
 	Config         *config.Config
 	AppTemplateDir string
 	Templates      embed.FS
+	ServiceName    string
 }
 
 type AppModel struct {
 	AppName        string
 	AppDescription string
+}
+
+type ServiceSpecs struct {
+	ServiceName string
+	Method      string
+	Spec        *openapi3.Operation
 }
 
 func (a *App) Execute(command string) error {
@@ -53,4 +62,41 @@ func (a *App) Execute(command string) error {
 	fmt.Println()
 	fmt.Printf(constants.ProjectBuiltMsg, a.AppModel.AppName)
 	return nil
+}
+
+func (a *App) GetServiceSpecs(service string) ([]*ServiceSpecs, error) {
+	pathsLen := len(a.Spec.Paths)
+
+	if service != nil {
+		pathsLen = 1
+	}
+
+	specs := make(map[string][]*ServiceSpecs)
+
+	for path, doc := range a.Spec.Paths {
+		specList := map[string]*openapi3.Operation{
+			http.MethodGet:    doc.Get,
+			http.MethodPost:   doc.Post,
+			http.MethodPatch:  doc.Patch,
+			http.MethodPut:    doc.Put,
+			http.MethodDelete: doc.Delete,
+		}
+
+		serviceName := ""
+		for method, spec := range specList {
+			if spec != nil {
+				if len(spec.Tags) == 0 {
+					return errors.New(constants.TagMissingErr)
+				}
+
+				if serviceName != "" {
+					if serviceName != spec.Tags[0] {
+						err := fmt.Sprintf(constants.TagDifferentErr)
+						return errors.New(constants.TagDifferentErr, spec.Tags[0], path)
+					}
+				}
+
+			}
+		}
+	}
 }
